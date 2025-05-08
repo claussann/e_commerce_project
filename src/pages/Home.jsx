22306d980c68bae4a6c59bfc01eb7d20ee8c1077
claussann <clausann92@gmail.com>
2025-05-08 12:09:14 +0200

    optimize grid component design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Button, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material"
 import { useState } from "react";
-import {ShoppingCart as ShoppingCartIcon} from '@mui/icons-material'
+import { ShoppingCart as ShoppingCartIcon } from '@mui/icons-material'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -13,24 +13,24 @@ function Home() {
             <Grid container spacing={2}>
                 <Grid size={12}>
                     <Button onClick={() => setOpen(true)}>SideBar</Button>
-                    <Typography variant="h1">e-commerce-project</Typography>
+                    <Typography variant="h1">eCommerce</Typography>
                 </Grid>
                 <Grid size={{ xs: 12, md: 10 }}>
                     <Typography>Product</Typography>
                 </Grid>
-                <Drawer open={open} onClose={setOpen(false)}>
+            </Grid>
+                <Drawer open={open} onClose={() => setOpen(false)}>
                     <List>
                         <ListItem>
                             <ListItemButton>
                                 <ListItemIcon>
                                     <ShoppingCartIcon />
                                 </ListItemIcon>
+                                <ListItemText primary="Cart" />
                             </ListItemButton>
-                            <ListItemText primary="Cart" />
                         </ListItem>
                     </List>
                 </Drawer>
-            </Grid>
         </>
     );
 }
